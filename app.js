0b767643a03c44e14a83d2b945e55f2355febccf
terminatorx8008 <vasu.namdevcs2021@indoreinstitute.com>
2024-01-22 15:37:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+function sendEmail() {
+    var name = document.querySelector("input[type='text']").value;
+    var email = document.querySelector("input[type='email']").value;
+    var subject = document.querySelector("#subject").value;
+    var message = document.querySelector("#message").value;
+    var recipient = "vasu.namdev.work@gmail.com"; // Replace with your email address
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: email,
+        Password: "your-email-password",
+        To: recipient,
+        From: email,
+        Subject: subject,
+        Body: message
+    }).then(
+        message => alert("Email sent successfully!")
+    ).catch(
+        error => alert("Failed to send email. Error: " + error)
+    );
+}
